d23b691722b5a599d33cecef790a69fdaaf50761
jesscru <jessicacrump7@gmail.com>
2020-10-31 00:48:07 -0600

    changed time start interval to 75000 ms; interval is not working

--- script.js
+++ script.js
@@ -5,19 +5,21 @@ var title = document.querySelector('.title');
 var timer = document.querySelector('#time-display')
 
 // other variables 
-var timeStart = 7500;
+var timeStart = 75000;
+// var changeToSec = timeStart / 1000;
 
 // start timer at 75secs when the start button is clicked 
 function startQuiz(){
-    var changeToSec = timeStart / 1000;
     var quizTime = setInterval(function(){
         timeStart--;
-        timer.textContent = 'Time: ' + changeToSec;
-    }, 7500)
+        timer.textContent = 'Time: ' + timeStart;
+    }, 75000)
 
     if (quizTime === 0) {
         clearInterval(quizTime);
     }
+    console.log(quizTime);
+    console.log(timeStart);
 }
 
 
