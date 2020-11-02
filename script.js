95004607a0c9ec51020f16043773084543d9fc82
jesscru <jessicacrump7@gmail.com>
2020-11-01 23:10:55 -0600

    added flex display to css to try to center text on homepage

--- script.js
+++ script.js
@@ -6,21 +6,19 @@ var timer = document.querySelector('#time-display')
 
 // other variables 
 var timeStart = 75000;
-// var changeToSec = timeStart / 1000;
+var changeToSec = timeStart / 1000;
 
 // start timer at 75secs when the start button is clicked 
-function startQuiz(){
+startButton.addEventListener('click', function startQuiz(){
     var quizTime = setInterval(function(){
         timeStart--;
-        timer.textContent = 'Time: ' + timeStart;
+        timer.textContent = 'Time: ' + changeToSec;
     }, 75000)
 
     if (quizTime === 0) {
         clearInterval(quizTime);
     }
-    console.log(quizTime);
+});
+
+    console.log(changeToSec);
     console.log(timeStart);
-}
-
-
-startButton.addEventListener('click', startQuiz());
