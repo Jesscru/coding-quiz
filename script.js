14f8483b0ee46214554278356867943b91ac35fb
jesscru <jessicacrump7@gmail.com>
2020-11-03 11:14:01 -0600

    added the object with the questions, answer choices, and correct answers to script

--- script.js
+++ script.js
@@ -1,23 +1,78 @@
-// variables to get html content 
+// button variables to access html content 
 var startBtn = document.querySelector(".button");
 var answerBtn = document.getElementsByClassName("answer-choices");
 var submitBtn = document.querySelector(".submit-button");
 var goBackBtn = document.querySelector("#go-back");
 var clearBtn = document.querySelector("#clear");
-
+// variables to access other html elements
 var title = document.querySelector(".title");
 var instructions = document.getElementsByClassName("instructions");
 var timer = document.getElementsByClassName("time-display");
 var question = document.getElementsByClassName("question");
-var correctAnswer = document.getElementById("true");
 var userInput = document.querySelector(".input")
 var userInitials = document.querySelector(".stored-user-initials");
 var allDone = document.querySelector('.all-done');
 
-
-// other variables 
+// js exclusive variables
 var timeStart = 75000;
 var changeToSec = timeStart / 1000;
+// each question saved in an object, answers nested inside question object, all stored in an array
+var quizContent = [
+    { 
+        question: 'Commonly used data types DO NOT include', 
+        answers: {
+            1: 'strings',
+            2: 'booleans',
+            3: 'alerts',
+            4: 'numbers'
+    }, 
+        correctAnswer: 3
+},
+
+    {
+        question2: 'The condition of an if / else statement is enclosed within _______.', 
+        answers2: {
+            1: 'quotes',
+            2: 'curly brackets',
+            3: 'parentheses',
+            4: 'square brackets'
+    }, 
+        correctAnswer: 3
+},
+    
+    {
+        question2: 'Arrays in JavaScripts can be used to store _______. ', 
+        answers2: {
+            1: 'numbers and strings',
+            2: 'other arrays',
+            3: 'booleans',
+            4: 'all of the above'
+    }, 
+        correctAnswer: 4
+},
+
+    {
+        question2: 'String values must be enclosed within _______ when being assigned to variables.', 
+        answers2: {
+            1: 'commas',
+            2: 'curly brackets',
+            3: 'quotes',
+            4: 'parentheses'
+    }, 
+        correctAnswer: 3
+},
+
+    {   
+        question2: 'A very useful tool used during development and debugging for printing content to the debugger is:', 
+        answers2: {
+            1: 'JavaScript',
+            2: 'terminal / bash',
+            3: 'for loops',
+            4: 'console.log'
+    }, 
+        correctAnswer: 4
+
+}];
 
 // start timer at 75secs when the start button is clicked 
 function startQuiz(){
