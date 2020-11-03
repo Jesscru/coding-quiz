8d0515b338d92362112df022c99ff9f8be34cbf6
jesscru <jessicacrump7@gmail.com>
2020-11-03 00:27:05 -0600

    added more document selectors, many of which are not working properly; started funcion for local storage of highscores

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 // variables to get html content 
-var startButton = document.getElementsByClassName('button');
-var instructions = document.getElementsByClassName('instructions');
-var title = document.getElementsByClassName('title');
-var timer = document.querySelector('#time-display');
-var question = document.getElementsByClassName('question');
-var answerBtn = document.getElementsByClassName('answer-choices');
-var correctAnswer = document.querySelector('#true');
+var startBtn = document.querySelector(".button");
+var instructions = document.getElementsByClassName("instructions");
+var title = document.querySelector(".title");
+var timer = document.getElementsByClassName("time-display");
+var question = document.getElementsByClassName("question");
+var answerBtn = document.getElementsByClassName("answer-choices");
+var correctAnswer = document.getElementById("true");
+var userInput = document.querySelector(".input")
 
 // other variables 
 var timeStart = 75000;
 var changeToSec = timeStart / 1000;
 
 // start timer at 75secs when the start button is clicked 
-startButton.onclick = function startQuiz(){
+function startQuiz(){
   
     var quizTime = setInterval(function(){
         timeStart--;
@@ -23,13 +24,38 @@ startButton.onclick = function startQuiz(){
         clearInterval(quizTime);
     }
 }
-// console.log(correctAnswer);
 
-answerBtn.addEventListener("click", function(event) {
+
+// when user clicks the correct answer, the question and answer choices change and time decrements if user answers incorrectly
+function detectAnswer(event) {
+    
     event.preventDefault();
 
-    if (event.target.matches(correctAnswer) {
-      question.textContent = 'The condition in an if / else statement is enclose within ______.'
+    if (event.target.matches(correctAnswer)) {
+      question.innerHTML = 'The condition in an if / else statement is enclose within ______.'
+        // display correct messaging and also change answer choices -- need variable help first 
+    } else {
+        timeStart - 15; 
+        // display wrong message and still change question and answer 
     }
-  });
+  };
 
+  console.log(startBtn);
+  console.log(instructions);
+  console.log(title);
+  console.log(timer);
+  console.log(question);
+  console.log(answerBtn);
+  console.log(correctAnswer);
+  console.log(userInput);
+
+
+// startBtn.addEventListener("click", startQuiz());
+// answerBtn.addEventListener("click", detectAnswer());
+
+
+// function storeHighscore(event){
+//     event.preventDefault();
+
+//     localStorage.setItem()
+// }
